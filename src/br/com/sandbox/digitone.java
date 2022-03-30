5e093d99b44d660d89cbc34b296b489120e0e482
Lombardi <lucaslombardi07@gmail.com>
2022-03-30 20:02:29 -0300

    First

---
+++ src/br/com/sandbox/digitone.java
@@ -0,0 +1,9 @@
+package br.com.sandbox;
+
+public class digitone {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello Wolrd! " + (a+b));
+    }
+}
